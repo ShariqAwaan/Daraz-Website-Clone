541480183c1338f9246aa6b025face5fe92e9dc6
Muhammad Shariq Abdullah <145561688+ShariqAwaan@users.noreply.github.com>
2023-12-19 16:40:36 +0500

    Add files via upload

---
+++ Daraz/nav.html/app.js
@@ -0,0 +1,10 @@
+window.onload()=function(pictures){
+
+
+}
+let a = document.getElementsByClassName("images");
+let b =document.getElementsByClassName("imgs")
+a.onclick=function(){
+    document.write=b[i].textContent="i am a image"
+
+}
